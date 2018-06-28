0e4b95da46a898c577a0c2f771fb4a99bf0f75fb
GarrusEn <Tinyismyson@gmail.com>
2018-06-28 19:25:58 +0500

    Обновление 28.06.18
    Добавлен функционал добавления вкладок.

--- TabControllApp/MainWindow.xaml.cs
+++ TabControllApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -18,7 +20,7 @@ namespace TabControllApp
         #region D&D
         // Drag&Drop 
         private void TabItem_Drag(object sender, MouseEventArgs e)
-        {           
+        {
             var tabItem = e.Source as TabItem;
 
             if (tabItem == null)
@@ -45,14 +47,14 @@ namespace TabControllApp
 
             return null;
         }
-                
+
         private void TabItem_Drop(object sender, DragEventArgs e)
         {
             var tabItemTarget = GetTargetTabItem(e.OriginalSource);
             if (tabItemTarget != null)
             {
                 var tabItemSource = (TabItem)e.Data.GetData(typeof(TabItem));
-                if (tabItemTarget != tabItemSource)
+                if (tabItemTarget != tabItemSource && tabs.Items.IndexOf(tabItemTarget) != tabs.Items.Count)
                 {
                     int sourceIndex = tabs.Items.IndexOf(tabItemSource);
                     int targetIndex = tabs.Items.IndexOf(tabItemTarget);
@@ -91,11 +93,49 @@ namespace TabControllApp
                 // Отступ 20 выбирался путём научного тыка
                 // Делим количество табов на ширину StackPanel, 
                 // отнимаем некоторый шаг, получаем нужный размер
-                double wd = (RenderSize.Width / tabs.Items.Count) - 20;
+                double wd = (RenderSize.Width / tabs.Items.Count) - 3;
                 if (wd < 20) wd = 20;
                 item.Width = wd;
             }
         }
+
+        #endregion
+
+        #region AddTabs
+        private void LastTab_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TabItem tabItem = new TabItem();
+            DockPanel dp = new DockPanel();
+            Button button = new Button();
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = RandomString(5);
+            dp.Children.Add(textBlock);
+            dp.Children.Add(button);
+            tabItem.Header = dp;
+
+            int count = tabs.Items.Count;
+            tabs.Items.Insert(count - 1, tabItem);
+
+            TabsFill();
+        }
+        #endregion
+
+        #region RandomNameTab
+        // Рандомизатор строки
+        private static Random random = new Random((int)DateTime.Now.Ticks);
+        private string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
         #endregion
     }
+
 }
